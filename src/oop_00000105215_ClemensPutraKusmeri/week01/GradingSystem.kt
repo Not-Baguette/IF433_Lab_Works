4d75290ad4a01ca5d7a74300877f963a48225987
Baguette <clemens.joe.gh@gmail.com>
2026-02-02 15:18:07 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000105215_ClemensPutraKusmeri.week01
+
+fun main(){
+  var name: String = "Clemens Putra Kusmeri"
+  var score: Int = 90
+
+  println("Nama: " + name + ", Score: " + score)
+}
