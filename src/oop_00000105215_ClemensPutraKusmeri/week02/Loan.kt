3a086a89fa10d5185753d9c434f4dea159d7f985
Baguette <clemens.joe.gh@gmail.com>
2026-02-09 16:41:06 +0700

    week02: (task 1) implement library input system

--- src/oop_00000105215_ClemensPutraKusmeri/week02/Loan.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week02/Loan.kt
@@ -1,12 +1,28 @@
 package oop_00000105215_ClemensPutraKusmeri.week02
+import java.util.Scanner
 
 class Loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 1){
   fun calculateFine() = if(loanDuration>3) ((loanDuration - 3) * 2000) else 0
 }
 
-/* TEST
 fun main(){
-  val book1 = Loan("the ones that walk away from omelas", "Naomi", 10)
+  val scanner = Scanner(System.`in`)
+  print("Masukkan Judul: ")
+  val title = scanner.nextLine()
+  print("Masukkan Nama Peminjam: ")
+  val borrower = scanner.nextLine()
+  print("Masukkan durasi pinjam: ")
+  var duration = scanner.nextInt()
+  scanner.nextLine()
+
+  if(duration<0) duration = 1
+  /*
+  do{
+    duration = scanner.nextInt()
+    scanner.nextLine()
+  }while(duration<0)
+  */
+
+  val book1 = Loan(title, borrower, duration)
   println(book1.calculateFine())
 }
-*/
