5f600ec93433e14cfef02a186685805d751344b0
Baguette <clemens.joe.gh@gmail.com>
2026-02-16 15:54:42 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week03/Player.kt
@@ -0,0 +1,28 @@
+package oop_00000105215_ClemensPutraKusmeri.week03
+
+
+class Player(val username: String){
+  private var xp: Int = 0
+  var level: Int = (xp/100) + 1
+
+  fun debugGetter(){
+    println("xp = $xp, level = $level, username = $username")
+  }
+
+  fun recheckLvl(){
+    var oldLvl = level
+    level = (xp/100) + 1
+
+    if(oldLvl != level) println("Level Up! Selamat $username naik ke level $level")
+  }
+
+  fun addXp(amount: Int){
+    if(amount<0){
+      println("Does not accept negative xp")
+    } else{
+      xp += amount
+      println("Increased XP by $amount, total XP is $xp")
+    }
+    recheckLvl()
+  }
+}
