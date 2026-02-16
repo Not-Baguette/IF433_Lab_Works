3e2c1dde62af4c24036ab3ef2ac69602138fe74e
Baguette <clemens.joe.gh@gmail.com>
2026-02-16 14:58:33 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000105215_ClemensPutraKusmeri/week03/Employee.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week03/Employee.kt
@@ -1,4 +1,9 @@
 package oop_00000105215_ClemensPutraKusmeri.week03
+
 class Employee (val name: String){
   var salary: Int = 0
+  set(value){
+    println("Mencoba untuk mengprint gaji $value")
+    this.salary = value
+  }
 }
