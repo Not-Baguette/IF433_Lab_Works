fdd9905c0f5ab34f20a0c0fb04a8a065b1db086d
Baguette <clemens.joe.gh@gmail.com>
2026-02-16 15:09:37 +0700

    week03: add private property

--- src/oop_00000105215_ClemensPutraKusmeri/week03/Employee.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week03/Employee.kt
@@ -1,6 +1,17 @@
 package oop_00000105215_ClemensPutraKusmeri.week03
 
 class Employee (val name: String){
+  private var performanceRating: Int = 3
+
+  fun increasePerformance(){
+    performanceRating++
+    println("Kinerja $name meningkat! Rating: $PerformanceRating")
+  }
+
+  fun printStatus{
+      println("Nama: $name, Rating: $PerformanceRating")
+  }
+
   var salary: Int = 0
   set(value){
     if(value<0){
