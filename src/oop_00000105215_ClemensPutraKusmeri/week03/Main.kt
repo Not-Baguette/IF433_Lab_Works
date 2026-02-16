41e6be9cd6ca640f712ca9e15af5cd967dffef45
Baguette <clemens.joe.gh@gmail.com>
2026-02-16 15:15:45 +0700

    week03: cleanup code

--- src/oop_00000105215_ClemensPutraKusmeri/week03/Main.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week03/Main.kt
@@ -1,8 +1,7 @@
 package oop_00000105215_ClemensPutraKusmeri.week03
 
 fun main(){
-  val e = Employee ("Budi")
-  println("--- Test Error ---")
+  val e = Employee("Budi")
 
   e.salary = -1000
   e.salary = 5000
