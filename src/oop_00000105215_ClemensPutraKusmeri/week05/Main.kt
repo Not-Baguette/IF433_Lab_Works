5c8a5cfa572f78b6d0fd3b87dc35f0b7104d8249
Baguette <clemens.joe.gh@gmail.com>
2026-03-02 14:01:43 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week05/Main.kt
@@ -0,0 +1,22 @@
+package oop_00000105215_ClemensPutraKusmeri.week05
+
+fun main(){
+  val dosen1 = Dosen(nama="Pak Alex", nidn="0123456")
+  val admin1 = Admin(nama="Bu Siti")
+
+  val daftarPegawai: List<Pegawai> = listof(dosen1, admin1)
+
+  println("=== Aktifitas Pegawai ===")
+  for(pegawai in daftarPegawai){
+    pegawai.bekerja()
+    when(pegawai){
+      is Dosen -> {
+        println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+        pegawai.mengajar()
+      } is Admin -> {
+        println("=> Terdeteksi sebagai Admin")
+        pegawai.doAdminWork()
+      }
+    }
+  }
+}
