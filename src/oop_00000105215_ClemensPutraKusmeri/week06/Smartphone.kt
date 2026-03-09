304734bb6c7f611a3dfe7aa65468526e6641919f
Baguette <clemens.joe.gh@gmail.com>
2026-03-09 11:58:41 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000105215_ClemensPutraKusmeri/week06/Smartphone.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week06/Smartphone.kt
@@ -1,3 +1,8 @@
 class smartwatch: Camera, Phone {
-  
+  // resolve
+  override fun turnOn(){
+    super<Camera>.turnOn()
+    super<Phone>.turnOn()
+    println("Sistem Operasi Smartphone berhasil booting.")
+  }
 }
