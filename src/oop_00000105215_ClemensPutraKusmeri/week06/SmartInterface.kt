598d16f23e3e5040df60336b0387801c4bffe923
Baguette <clemens.joe.gh@gmail.com>
2026-03-09 12:30:52 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week06/SmartInterface.kt
@@ -0,0 +1,42 @@
+package oop_00000105215_ClemensPutraKusmeri.week06
+
+interface SmartDevice{
+  val id: String
+  val name: String
+}
+
+interface Switchable{
+  abstract fun turnOn()
+  abstract fun turnOff()
+}
+
+interface Recordable{
+  abstract fun startRecord()
+  fun stopRecord(){ println("Perekaman dihentikan dan disimpan ke Cloud.") }
+}
+
+/*
+class Test: SmartDevice, Switchable, Recordable{
+  override val id: String = "67"
+  override val name: String = "Donovan Premium"
+  
+  override fun turnOn(){
+    println("test1")
+  }
+
+  override fun turnOff(){
+    println("test2")
+  }
+  
+  override fun startRecord(){
+    println("Perekaman dimulai")
+  }
+}
+
+fun main(){
+  val ie = Test()
+  ie.turnOn()
+  ie.turnOff()
+}
+
+*/
