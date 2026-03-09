3d56a98bc25e5d9906d633c78f17276016456488
Baguette <clemens.joe.gh@gmail.com>
2026-03-09 11:51:21 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week06/Smartwatch.kt
@@ -0,0 +1,13 @@
+class smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+  override fun showTime(){
+    println("Layar OLED menyala: 14:00 WIB")
+  }
+
+  override fun connectBluetooth(){
+    println("Mencari perangkat HP di sekitar untuk Pairing...")
+  }
+
+  override fun chargeBattery(){
+    println("Mengisi daya menggunakan charger magnetik 15W")
+  }
+}
