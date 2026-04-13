6981d39a7c85b85916a3e4e9f2170e5ef42f9903
Baguette <clemens.joe.gh@gmail.com>
2026-04-13 13:33:12 +0700

    week08: simulate unit test failure using !!

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week08/UserTest.kt
@@ -0,0 +1,7 @@
+package oop_00000105215_ClemensPutraKusmeri.week08
+
+fun runMockUnitTest() {
+  println("Running simulated unit test...")
+  val expectedResult: String? = null
+  println(expectedResult!!.length)
+}
