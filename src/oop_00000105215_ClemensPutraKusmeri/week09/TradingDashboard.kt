4243907db51bcc773e62a280f9232829638d805e
Baguette <clemens.joe.gh@gmail.com>
2026-04-20 14:58:27 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000105215_ClemensPutraKusmeri/week09/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_00000105215_ClemensPutraKusmeri.week09
+
+fun main(){
+  val tradeHistory = listOf(
+    TradeLog("BTCUSDT", "LONG", 1000, 500, "OPEN")
+    TradeLog("BTCUSDT", "SHORT", 2000, -1000, "CLOSED")
+    TradeLog("ETHUSDT", "SHORT", 3000, 500, "OPEN")
+    TradeLog("BTCUSDT", "SHORT", 4000, 1000, "CLOSED")
+    TradeLog("BTCUSDT", "LONG", 5000, -500, "OPEN")
+    TradeLog("BTCUSDT", "LONG", 2200, 1000, "CLOSED")
+  )
+
+}
