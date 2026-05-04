0e6e28000ffdff5ea57b2b938a6c3b0a45faa0bb
Baguette <clemens.joe.gh@gmail.com>
2026-05-04 13:59:21 +0700

    week11: (task) implement device search with let

--- src/oop_00000105215_ClemensPutraKusmeri/week11/SmartHomeDashboard.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week11/SmartHomeDashboard.kt
@@ -24,4 +24,10 @@ fun main() {
     homeDevices.add(acInverter)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n--- Hasil Pencarian Kamera ---")
+        println(it.diagnose())
+    }
 }
