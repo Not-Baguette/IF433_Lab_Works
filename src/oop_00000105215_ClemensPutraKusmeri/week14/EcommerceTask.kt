6ba08137f3bef1de4204f4b491c52c7733039995
Baguette <clemens.joe.gh@gmail.com>
2026-05-25 12:40:01 +0700

    week14: create OrderStorage and NotificationService abstractions

--- src/oop_00000105215_ClemensPutraKusmeri/week14/EcommerceTask.kt
+++ src/oop_00000105215_ClemensPutraKusmeri/week14/EcommerceTask.kt
@@ -2,6 +2,27 @@ package oop_00000105215_ClemensPutraKusmeri.week14
 
 import java.io.File
 
+interface OrderStorage {
+    fun save(itemName: String, finalPrice: Double, customerType: String)
+}
+
+class FileOrderStorage : OrderStorage {
+    private val file = File("orders.csv")
+    override fun save(itemName: String, finalPrice: Double, customerType: String) {
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+    }
+}
+
+interface NotificationService {
+    fun notify(itemName: String)
+}
+
+class EmailNotificationService : NotificationService {
+    override fun notify(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
 class BadOrderProcessor {
     // VIOLATION: Hardcoded File I/O (DIP), Melakukan kalkulasi + I/O + Notifikasi sekali
     private val file = File("orders.csv")
